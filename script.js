7b09f6e6d30abbd71c99ec324da80ce93dc21b48
Ronan-dev-sudo <rockyj763@gmail.com>
2026-07-29 00:09:33 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,40 @@
 function showMessage() {
     document.getElementById("message").innerHTML =
-    "May your day be filled with happiness, blessings, and amazing memories 💙🎉";
+    "Happy Birthday 💗 May your day be full of love, laughter and beautiful memories ✨";
+}
+
+
+function xoKitty() {
+    document.body.style.background =
+    "linear-gradient(135deg, #ffd6e7, #ffb6d9)";
+
+    document.getElementById("message").innerHTML =
+    "🎀 Welcome to XO Kitty K-Drama Core 💗✨";
+}
+
+
+function kitkat() {
+    document.body.style.background =
+    "linear-gradient(135deg, #8b0000, #ff4d4d)";
+
+    document.getElementById("message").innerHTML =
+    "🍫 Sweet moments, sweet memories. KitKat theme activated ❤️";
+}
+
+
+function puppy() {
+    document.body.style.background =
+    "linear-gradient(135deg, #fff1c1, #ffd6a5)";
+
+    document.getElementById("message").innerHTML =
+    "🐶 Puppy mode activated! Cute energy only 🐾";
+}
+
+
+function netflix() {
+    document.body.style.background =
+    "linear-gradient(135deg, #000000, #8b0000)";
+
+    document.getElementById("message").innerHTML =
+    "🎬 A Project Bliss Original. Episode 1 begins...";
 }
